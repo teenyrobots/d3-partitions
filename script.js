b08b48624014ce19cb31a9e6bbb8d60976bb5d57
Joyce <hellojoycerice@gmail.com>
2019-10-16 16:26:18 -0400

    uses a polylinear scale to scale the parts of the partition

--- script.js
+++ script.js
@@ -12,10 +12,15 @@ var vData = {
     }]
 };
 
-var vWidth = 940;  // <-- 1
-var vHeight = 940;
+var vWidth = 925;  // <-- 1
+var vHeight = 925;
 var vRadius = Math.min(vWidth, vHeight) / 2;  // < -- 2
 var vColor = d3.scaleOrdinal(d3.schemeCategory20b);   // <-- 3
+
+//scales the rings: https://stackoverflow.com/questions/37684415/d3-sunburst-how-to-set-different-ring-level-widths buuut just makes it tiny
+var rscale = d3.scaleLinear()
+ .domain([0,(vRadius * .73),vRadius])
+ .range([0,(vRadius * .98),vRadius]);
 
 var g = d3.select('svg')  // <-- 1
     .attr('width', vWidth)  // <-- 2
@@ -34,8 +39,18 @@ var vRoot = d3.hierarchy(theData)  // <--1
 var vArc = d3.arc()
     .startAngle(function (d) { return d.x0 })
     .endAngle(function (d) { return d.x1 })
-    .innerRadius(function (d) { return vRadius - d.y1 + vRoot.y1; })
-    .outerRadius(function (d) { return vRadius - d.y0 + vRoot.y1; });
+//normal
+//    .innerRadius(function (d) { return d.y0 })
+//    .outerRadius(function (d) { return d.y1 });
+
+//equal sized rings, flipped sunburst
+    // .innerRadius(function (d) { console.log("inner radius: " + (vRadius - d.y1 + vRoot.y1)); return vRadius - d.y1 + vRoot.y1; })
+    // .outerRadius(function (d) { console.log("outer radius " + (vRadius - d.y0 + vRoot.y1)); return vRadius - d.y0 + vRoot.y1; });
+
+//applies the scale but just makes it tiny :(
+   .innerRadius(function (d) { console.log("inner radius: " + rscale(vRadius - d.y1 + vRoot.y1)); return rscale(vRadius - d.y1 + vRoot.y1); })
+   .outerRadius(function (d) { console.log("outer radius " + rscale(vRadius - d.y0 + vRoot.y1)); return rscale(vRadius - d.y0 + vRoot.y1); });
+
 
 var vNodes = vRoot.descendants();  // <--3
 
